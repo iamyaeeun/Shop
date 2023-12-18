57a781075887a4d894584fb9464191dfd07d9a0c
iamyaeeun <108451447+iamyaeeun@users.noreply.github.com>
2023-12-18 21:08:07 +0900

    Update MemberForm.java

--- src/main/java/opensource/shop/controller/MemberForm.java
+++ src/main/java/opensource/shop/controller/MemberForm.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.NotEmpty;
 @Getter @Setter
 public class MemberForm {
 
-    @NotEmpty(message = "회원 이름은 필수입니다!")
+    @NotEmpty(message = "회원 이름은 필수입니다!!")
     private String name; //이름
 
     private String city; //도시
